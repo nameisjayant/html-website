ad8c8d0e94907625f72d5bbec7072ec536f487b3
nameisjayant <jayant@antino.com>
2025-01-24 15:35:56 +0530

    feat : added mulitple navigation

--- src/OpenApp.jsx
+++ src/OpenApp.jsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const OpenApp = () => {
+  const handleButtonClick = (key) => {
+    const appUrl = key ? `coworking-app://${key}` : `coworking-app://`;
 
-  const handleButtonClick = () => {
-    window.location.href = "coworking-app://dashboard"
-    setTimeout(()=>{
-        window.location.href = "https://play.google.com/store/apps/details?id=com.awfis.mobile"
-    },2000)
+    // Attempt to open the app
+    window.location.href = appUrl;
+
+    // Fallback to Play Store after 2 seconds
+    setTimeout(() => {
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.awfis.mobile";
+    }, 2000);
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to the website</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{"Welcome to the website"}</h1>
       <button
-        onClick={handleButtonClick}
+        onClick={() => handleButtonClick("")}
         style={{
-          padding: '10px 20px',
-          fontSize: '16px',
-          cursor: 'pointer',
-          marginTop: '20px',
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+          marginTop: "20px",
         }}
       >
-        {'Open Awfis App'}
+        {"Open Home Page"}
+      </button>
+      <button
+        onClick={() => handleButtonClick("meetingRoom")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+          marginTop: "20px",
+          marginLeft: "20px",
+        }}
+      >
+        {"Open Meeting Room Page"}
+      </button>
+      <button
+        onClick={() => handleButtonClick("fnb")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+          marginTop: "20px",
+          marginLeft: "20px",
+        }}
+      >
+        {"Open F&B Page"}
+      </button>
+      <button
+        onClick={() => handleButtonClick("daypass")}
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+          marginTop: "20px",
+          marginLeft: "20px",
+        }}
+      >
+        {"Open DayPass Page"}
       </button>
     </div>
   );
